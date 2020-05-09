32d1c1a2ceb693eeb97e90e4c94ce9829d85209f
MahonriM <cripto3000z@gmail.com>
2020-05-09 14:23:12 -0500

    Create Condion.js

---
+++ Condion.js
@@ -0,0 +1,8 @@
+let numero = parseInt(prompt("Ingrese la calificacion:"));
+var nombr= prompt("Ingres tu numbre");
+if (numero>=70){
+console.log("Haz aprobado "+nombr);  
+}
+else{
+  console.log("Haz reprobado "+nombr);
+}
